f75a0c1712ae8571cb984505e0844c4f28599aeb
Nitin Goyal <goyalnitin92566@gmail.com>
2024-10-26 16:14:26 +0530

    Printing all Numbers Given in List

--- ELements.py
+++ ELements.py
@@ -1,7 +1,15 @@
 #Ques- Print the elements of the following list using a loop:
 
+# list=[1,7,2,6,10,5,190]
+# i = 0
+# for i in range(len(list)):
+#   print(list[i],end=" ")
+#   i = i + 1
+
+# We can also do this by for loop as well
+
 list=[1,7,2,6,10,5,190]
 i = 0
-for i in range(len(list)):
+while(i<len(list)):
   print(list[i],end=" ")
-  i = i + 1
+  i+=1
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+list=[1,7,2,6,10,5,190]
+# i = 0
+# for i in range(len(list)):
+#   print(list[i],end=" ")
+#   i = i + 1
