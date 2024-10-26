93ad6d83de40f1bbe2371fb96ed5558ee59b0f60
Nitin Goyal <goyalnitin92566@gmail.com>
2024-10-26 15:55:16 +0530

    Printing All Numbers Using Loops

---
+++ PrintNumber.py
@@ -0,0 +1,20 @@
+# initialize a i
+
+i = 0
+
+# Apply for while loop
+
+while(i<=100):
+  print(i,end=" ")
+  i = i + 1
+print("DONE")
+
+  # We can also do this by for loop as well
+i = 1
+
+# apply for loop
+
+for i in range(1,101):
+  print(i,end= " ")
+  
+
