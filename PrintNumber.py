2b29d871d6e6c70621ea5e72fc4117d6c66cbf0d
Nitin Goyal <goyalnitin92566@gmail.com>
2024-10-26 16:29:25 +0530

    Printing all Numbers using loops

--- PrintNumber.py
+++ PrintNumber.py
@@ -1,20 +1,34 @@
-# initialize a i
+# # initialize a i
 
 i = 0
 
-# Apply for while loop
+# # Apply for while loop
 
 while(i<=100):
   print(i,end=" ")
   i = i + 1
 print("DONE")
 
-# We can also do this by for loop as well
+# # We can also do this by for loop as well
 i = 1
 
-# apply for loop
+# # apply for loop
 
 for i in range(1,101):
   print(i,end= " ")
+
+# if we want to print Numbers From Reverse Order like 100 to 1
+
+i = 100
+while i>=1: #or while i!=0:
+  print(i,end=" ")
+  i-=1
   
+  # BY the way of for loop
+
+i = 100
+for i in range(100,0,-1):
+    print(i,end=" ")
+    
+
 
