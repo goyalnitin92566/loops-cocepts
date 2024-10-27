7238f9045c4128352aa36ca322edda47c22ea209
Nitin Goyal <goyalnitin92566@gmail.com>
2024-10-27 08:48:26 +0530

    Continue And Break Statement

---
+++ BreakandContinue.py
@@ -0,0 +1,16 @@
+#break Statement
+i = 0 
+while  i <=10:
+  if i == 9:
+    break
+  print(i)
+  i+=1
+
+#continue statement
+i = 0
+while i <=10:
+ if i == 7:
+   i+=1
+   continue
+ print(i)
+ i+=1
